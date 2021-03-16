c75a73ed91ead55d39a7bf5179fa64a9d75ed3e1
jeel-a-simformsolutions <jeel.a@simformsolutions.com>
2021-03-16 12:24:57 +0530

    first commit

---
+++ src/data/SliderData.js
@@ -0,0 +1,41 @@
+import HomeOne from '../images/HomeOne.jpg';
+import HomeTwo from '../images/HomeTwo.jpg';
+import HomeThree from '../images/HomeThree.jpg';
+import HomeFour from '../images/HomeFour.jpg';
+
+
+export const SliderData = [
+    {
+        title: 'Luxary villa in Bali, Indonesia',
+        price: '$4,280,000',
+        path: '/home',
+        label: 'view Home',
+        image: HomeOne,
+        alt: 'House'
+    },
+    {
+        title: 'House in San Jose, Costa Rica',
+        price: '$2,860,000',
+        path: '/home',
+        label: 'view Home',
+        image: HomeTwo,
+        alt: 'House'
+    },
+    {
+        title: 'House in Calabasas, Califormia',
+        price: '$8,420,000',
+        path: '/home',
+        label: 'view Home',
+        image: HomeThree,
+        alt: 'House'
+    },
+    {
+        title: 'House in Houston, Texas',
+        price: '$6,660,000',
+        path: '/home',
+        label: 'view Home',
+        image: HomeFour,
+        alt: 'House'
+    },
+
+]
